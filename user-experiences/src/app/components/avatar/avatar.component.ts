f8623be7697d50f77a726e16f7a7fb3c74471f4e
Ruslan <61955096+ruslanpascoal2@users.noreply.github.com>
2022-11-12 14:32:05 -0300

    getting the file

--- user-experiences/src/app/components/avatar/avatar.component.ts
+++ user-experiences/src/app/components/avatar/avatar.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AvatarComponent implements OnInit {
 
+  file: File | null = null;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onFileChange(event: any){
+    const files = event.target.files as FileList;
+    
+    if(files.length > 0){
+      this.file = files[0];
+    }
+  }
+ 
 }
