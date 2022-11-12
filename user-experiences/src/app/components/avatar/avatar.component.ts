99aa5e09149731e577391be7c868553e2e70d3fb
Ruslan <61955096+ruslanpascoal2@users.noreply.github.com>
2022-11-12 19:39:04 -0300

    image cropper component

--- user-experiences/src/app/components/avatar/avatar.component.ts
+++ user-experiences/src/app/components/avatar/avatar.component.ts
@@ -1,26 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
+import { ImageCropperComponent } from './components/image-cropper/image-cropper.component';
 
 @Component({
   selector: 'app-avatar',
   templateUrl: './avatar.component.html',
-  styleUrls: ['./avatar.component.scss']
+  styleUrls: ['./avatar.component.scss'],
 })
 export class AvatarComponent implements OnInit {
+  file: string = '';
 
-  file: string = ""; //it will be a base64 string
+  constructor(public dialog: MatDialog) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
-  onFileChange(event: any){
+  onFileChange(event: any) {
     const files = event.target.files as FileList;
-    
-    if(files.length > 0){
-      this.file = URL.createObjectURL(files[0]);
+
+    if (files.length > 0) {
+      const _file = URL.createObjectURL(files[0]);
+      this.openAvatarEditor(_file)
+      .subscribe(
+        (result) => {
+          console.log(result);
+        }
+      )
     }
   }
 
- 
+  openAvatarEditor(image: string): Observable<any> {
+    const dialogRef = this.dialog.open(ImageCropperComponent, {
+      maxWidth: '80vw',
+      maxHeight: '80vh',
+      data: image,
+    });
+
+    return dialogRef.afterClosed();
+  }
 }
