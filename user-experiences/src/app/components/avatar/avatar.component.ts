522e0f211a7ee00121a08bd8e091289d01744105
Ruslan <61955096+ruslanpascoal2@users.noreply.github.com>
2022-11-13 11:57:12 -0300

    control value accessor

--- user-experiences/src/app/components/avatar/avatar.component.ts
+++ user-experiences/src/app/components/avatar/avatar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { Observable } from 'rxjs';
 import { ImageCropperComponent } from './components/image-cropper/image-cropper.component';
@@ -7,13 +8,41 @@ import { ImageCropperComponent } from './components/image-cropper/image-cropper.
   selector: 'app-avatar',
   templateUrl: './avatar.component.html',
   styleUrls: ['./avatar.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: AvatarComponent
+    }
+  ]
 })
-export class AvatarComponent implements OnInit {
+export class AvatarComponent implements OnInit, ControlValueAccessor {
   file: string = '';
 
   constructor(public dialog: MatDialog) {}
 
   ngOnInit(): void {}
+
+  writeValue(_file: string): void {
+    this.file = _file;
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  onChange = (fileUrl: string) => {
+    console.log(fileUrl);
+  };
+
+  onTouched = () => {};
+
+  disabled: boolean = false;
 
   onFileChange(event: any) {
     const files = event.target.files as FileList;
@@ -26,6 +55,7 @@ export class AvatarComponent implements OnInit {
         (result) => {
           if(result){
             this.file = result;
+            this.onChange(this.file);
           }
         }
       )
--- user-experiences/src/app/pages/profile/profile.component.ts
+++ user-experiences/src/app/pages/profile/profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -19,6 +19,10 @@ export class ProfileComponent implements OnInit {
       email: '',
       avatar: ''
     })
+  }
+
+  submit(){
+    console.log(this.form.value);
   }
 
 }
